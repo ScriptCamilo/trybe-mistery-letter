c7e76dabeaaed8b8f14c2192490964b4c2323395
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-26 09:28:17 -0300

    Third and fourth requirements done

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// function createSpanLetter(text) {
-//   const span = document.createElement('span');
-//   span.innerText = text;
+function createSpanLetter(text) {
+  const span = document.createElement('span');
+  span.innerText = text;
 
-//   return span;
-// }
+  return span;
+}
 
-// function setLetter() {
-//   const input = document.querySelector('#carta-texto').value.split(' ');
-//   const letter = document.querySelector('#carta-gerada');
+function setLetter() {
+  const input = document.querySelector('#carta-texto').value.split(' ');
+  const letter = document.querySelector('#carta-gerada');
 
-//   letter.innerText = '';
+  letter.innerText = '';
 
-//   input.forEach((text, index) => {
-//     const word = createSpanLetter(text);
+  input.forEach((text, index) => {
+    const word = createSpanLetter(text);
 
-//     letter.appendChild(word);
-//     if (input.length === index + 1) letter.innerHTML += '.';
-//     else letter.innerHTML += ' ';
-//   });
-// }
+    letter.appendChild(word);
+    if (input.length === index + 1) letter.innerHTML += '.';
+    else letter.innerHTML += ' ';
+  });
+}
 
-// function getEvents() {
-//   const button = document.querySelector('#criar-carta');
+function getEvents() {
+  const button = document.querySelector('#criar-carta');
 
-//   button.addEventListener('click', setLetter);
-// }
+  button.addEventListener('click', setLetter);
+}
 
-// window.onload = () => {
-//   getEvents();
-// };
+window.onload = () => {
+  getEvents();
+};
